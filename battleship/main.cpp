306d7d069ef7d9487e9baec76c510494813c0cbc
adi-exe <mail.adithyanp@gmail.com>
2024-12-07 20:00:42 +0530

    enhanced menu s/m , intro fn

--- battleship/main.cpp
+++ battleship/main.cpp
@@ -11,7 +11,7 @@ std::string reset = "\033[0m";             // reset back to white colour
 void print_banner();                       // print banner function
 void clear();                              // clear screen function 
 char print_map(char ship[10][10]);         // print input map 
-void start_game();  
+int substart();  
 int main();                       // start game menu function 
 
 
@@ -113,60 +113,117 @@ char print_map(char ship[10][10]){    // function to print a particular map
     }
     cout<<"\n";
   }
+  return 0;
 }
 
-void start_game(){
-  int op = 0;
+int substart(){
   clear();
   print_banner();
-  cout<<green<<" [1] - PLACE SHIPS \n [2] - REPLACE/RE-ORDER SHIPS \n [3] - START GAME \n [4] - RENAME PLAYER \n [5] - BACK TO MAIN MENU \n\n -> ENTER GAME OPTION :"<<reset;
-  cin>>op;
-  if(op==1){
-    // get into place ship function one by one 
-  }
-  if(op==2){
-    //get into specific player to reorder their ships -> completely -> partially 
-  }
-  if(op==3){
-    // continue to further start game 
-  }
-  if(op==4){
-    // rename players .
-  }
-  if(op==5){
-    // go back to main menu 
-    main();
-  }
-  else{
-    cout<<"INVALID OPTION START AGANIN ";
-  }
+  cout<<yellow<<" NOW , TYPE IN PLAYER-1 NAME : ";
+  cin>>pl1;
+  cout<<" NOW , TYPE IN PLAYER-2 NAME : ";
+  cin>>pl2;
+  cout<<green<<"\n SUCESSFULLY UPDATED NAME OF PLAYERS  \n\n "<<yellow<<" PLAYER 1 : "<<pl1<<"\n  PLAYER 2 : "<<pl2;
+  cin.ignore();
+  cin.get();
+  
+    while(1){
+      int rp=0;
+      clear();
+      print_banner();
+      cout<<green<<" [1] - START GAME  \n [2] - REPLACE/RE-ORDER SHIPS \n [3] - VIEW MAP \n [4] - RENAME PLAYER \n [5] - BACK TO MAIN MENU \n"<<reset;
+      int op2 = 0;
+      cout<<green<<"\n -> ENTER GAME OPTION : "<<reset;
+      cin>>op2;
+      switch(op2) {
+        case 1:
+            cout<<yellow<<"GOING TO START GAME , PRESS ANY KEY TO CONTINUE ......"<<reset;
+            cin.ignore();
+            cin.get();
+            // get into place ship function one by one 
+            cout<<"not implemented";
+            break;
+    
+        case 2:
+            //get into specific player to reorder their ships -> completely -> partially 
+            cout<<yellow<<"STARTING TO REORDER SHIP , PRESS ANY KEY TO CONTINUE ......"<<reset;
+            cin.ignore();
+            cin.get();
+            cout<<"not implemented";
+            break;
+    
+        case 3:
+            // view player map
+            clear();
+            print_banner(); 
+            cout<<green<<" WHICH MAP TO VIEW ? \n [1] - "<<pl1<<"\n [2] - "<<pl2<<"\n ENTER OPTION "<<reset;
+            cin>>rp;
+            cout<<"TELL OTHER PLAYER TO MOVE ! , PRESS ANY KEY TO CONTINUE ..... ";
+            cin.ignore();
+            cin.get();
+            if(rp==1) print_map(a1);
+            if(rp==2) print_map(a2);
+            cout<<"PRESS ANY KEY TO CLEAR DISPLAYED MAP......";
+            cin.ignore();
+            cin.get();
+            break;
+    
+        case 4:
+            // rename players .
+            clear();
+            print_banner();
+            cout<<yellow<<"ENTER NAME OF PLAYER 1 : ";
+            cin>>pl1;
+            cout<<"ENTER NAME OF PLAYER 2 : ";
+            cin>>pl2;
+            cout<<"PLAYER NAME CHANGED SUCSSFULLY \n PLAYER-1 :"<<pl1<<"\n PLAYER-2 :"<<pl2<<"\n press any key to continue"<<reset;
+            cin.ignore();
+            cin.get();
+            break;
 
+        case 5:
+            // go back to main menu 
+            return 0;
+            
+        default:
+            break;
+    }
+  }
 }
 
 
 int main(){
-  int op = 0;
-  clear();
-  print_banner();
-  while (true)
+
+  while (1)
   {
+    clear();
+    print_banner();
+    int op = 0;
     cout<<green<<" [1] - START GAME \n [2] - VIEW GAME RULES \n [3] - QUIT GAME \n\n -> ENTER GAME OPTION :"<<reset;
     cin>>op;
-    if(op==1){
-      // get into start game function 
-      start_game();
-    }
-    if(op==2){
-      //get into view rules function 
-    }
-    if(op==3){
-      cout<<red<<"EXITING THE GAME ! \n"<<reset;
-      break;
-      break;
-    }
-    else{
-      cout<<"INVALID INPUT \n";
-    }
-  }
+    switch(op){
+      case 1:
+        cout<<yellow<<"GOING TO START THE GAME , FOLLOW INSTRUCRIONS , PRESS ANY KEY TO CONTINUE ....."<<reset;
+        cin.ignore();
+        cin.get();
+        // go to substart function
+        substart();
+        cout<<"returned to main funcrujn";
+        break;
     
+      case 2:
+        // go to view rule function 
+        cout<<"not implemeted";
+        break;
+    
+      case 3:
+        cout<<red<<"EXITING THE GAME ! \n"<<reset;
+        return 0;
+    
+      default:
+        cout<<"INVALID INPUT TRY AGAIN from main function  \n";
+        break;
+    }
+  } 
 }
+  
